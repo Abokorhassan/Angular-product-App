a94cb8a05902bbcf08a6151f865a093963ea7b84
abokorhassan <abokor13@hotmail.com>
2020-10-02 03:35:49 -0700

    Prepared server for production.

--- server/index.js
+++ server/index.js
@@ -9,10 +9,11 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
-    winston.info(`Listening on port ${port}...`)
+  winston.info(`Listening on port ${port}...`)
 );
 
 module.exports = server;
